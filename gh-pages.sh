9df2a3fcdf49a4f42fae9b01cce1a32acc694b33
Francisco <franciscoigor@gmail.com>
2019-08-22 17:11:39 -0700

    gh-pages full script

--- gh-pages.sh
+++ gh-pages.sh
@@ -1,1 +1,4 @@
+git branch -D gh-pages 
+git push origin --delete gh-pages
 git checkout -b gh-pages && npm run build && git add -f dist && git commit -m 'gh-pages' && git push origin gh-pages 
+git checkout master
