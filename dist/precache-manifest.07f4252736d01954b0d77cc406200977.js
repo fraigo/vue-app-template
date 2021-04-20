207fcc8639f14e7e38e574360a6dc62e708f2f10
Francisco Igor <franciscoi@tbadigital.com>
2021-04-20 16:52:48 -0700

    gh-pages

---
+++ dist/precache-manifest.07f4252736d01954b0d77cc406200977.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da426dbf7e8dcba9925f",
+    "url": "css/about.79a668c6.css"
+  },
+  {
+    "revision": "977ed3344d24f140730d",
+    "url": "css/app.457d62b7.css"
+  },
+  {
+    "revision": "b5db97d1742e7d0728b0",
+    "url": "css/chunk-vendors.56e112bf.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "1ccfba86f919cd2fe0305bd0c5350cd9",
+    "url": "index.html"
+  },
+  {
+    "revision": "da426dbf7e8dcba9925f",
+    "url": "js/about.41595688.js"
+  },
+  {
+    "revision": "977ed3344d24f140730d",
+    "url": "js/app.4b32181b.js"
+  },
+  {
+    "revision": "b5db97d1742e7d0728b0",
+    "url": "js/chunk-vendors.646b2301.js"
+  },
+  {
+    "revision": "7d06071885cf6ddc66e76366e199ca33",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
