0faeca32c0668c3cb3673a6a1315f1635c4d48bc
Francisco Igor <franciscoi@tbadigital.com>
2021-04-20 15:51:22 -0700

    sass config update

--- src/App.vue
+++ src/App.vue
@@ -8,22 +8,12 @@
   </div>
 </template>
 
+<script>
+import '@/assets/scss/fonts.scss'
+import '@/assets/scss/app.scss'
+export default {
+}
+</script>
+
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
