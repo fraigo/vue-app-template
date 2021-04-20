8a3dc12d09b06504c241b90784ea36e20343e8cb
Francisco Igor <franciscoi@tbadigital.com>
2021-04-20 16:47:22 -0700

    store example

--- src/store.js
+++ src/store.js
@@ -3,14 +3,32 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+const defaultState = {
+  lastDate: ''
+}
+
+const ls = localStorage
+const prefix = 'app.'
+for (const key in defaultState) {
+  if (ls.getItem(prefix + key)) {
+    defaultState[key] = ls.getItem(prefix + key)
+  }
+}
+console.log(defaultState)
+
 export default new Vuex.Store({
-  state: {
-
-  },
+  state: defaultState,
   mutations: {
-
+    trackDate: function (state) {
+      state.lastDate = (new Date()).getTime()
+      this.dispatch('store')
+    }
   },
   actions: {
-
+    store: function () {
+      for (const key in defaultState) {
+        ls.setItem(prefix + key, defaultState[key])
+      }
+    }
   }
 })
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/img/logo.png">
-    <Content msg="Welcome to Your Vue.js App"/>
+    <Content msg="Welcome to Your Vue.js App!"/>
+    <div>Last access: {{last}} sec.</div>
   </div>
 </template>
 
@@ -11,8 +12,21 @@ import Content from '@/components/Content.vue'
 
 export default {
   name: 'home',
+  data: function () {
+    return {
+      last: 0
+    }
+  },
   components: {
     Content
+  },
+  mounted: function () {
+    if (this.$store.state.lastDate > 0) {
+      this.last = (new Date() - new Date(this.$store.state.lastDate * 1)) / 1000
+    }
+    this.$store.commit('trackDate')
+  },
+  computed: {
   }
 }
 </script>
